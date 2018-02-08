777226ca56c3ade2d308cb9a8286e6563adaf3a4
ImaginarioColectivo <marjourzua@gmail.com>
2018-02-07 21:47:29 -0300

    funcionalidad de trazar ruta

---
+++ assets/js/index.js
@@ -0,0 +1,103 @@
+function initMap() {
+  var uluru = {lat: -33.45, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+
+function buscar(){
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+  };
+};
+
+document.getElementById("encuentrame").addEventListener("click", buscar);
+var latitud, longitud;
+
+var funcionExito = function(position) {
+    latitud = position.coords.latitude;
+    longitud = position.coords.longitude;
+
+    var miUbicacion = new google.maps.Marker({
+    position: {lat:latitud, lng:longitud},
+    animation: google.maps.Animation.DROP,
+    map: map
+});
+    map.setZoom(18);
+    map.setCenter({lat:latitud, lng:longitud});
+  };
+
+var funcionError = function (error) {
+  alert("Tenemos un problema con encontrar tu ubicación");
+  }
+
+new AutocompleteDirectionsHandler(map);
+
+}
+
+function AutocompleteDirectionsHandler(map) {
+        this.map = map;
+        this.originPlaceId = null;
+        this.destinationPlaceId = null;
+        this.travelMode = 'WALKING';
+        var originInput = document.getElementById('origin-input');
+        var destinationInput = document.getElementById('destination-input');
+        var modeSelector = document.getElementById('mode-selector');
+        this.directionsService = new google.maps.DirectionsService;
+        this.directionsDisplay = new google.maps.DirectionsRenderer;
+        this.directionsDisplay.setMap(map);
+
+        var originAutocomplete = new google.maps.places.Autocomplete(
+            originInput, {placeIdOnly: true});
+        var destinationAutocomplete = new google.maps.places.Autocomplete(
+            destinationInput, {placeIdOnly: true});
+
+        this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+        this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+      }
+      // Autocomplete.
+      AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
+        var radioButton = document.getElementById(id);
+        var me = this;
+        radioButton.addEventListener('click', function() {
+          me.travelMode = mode;
+          me.route();
+        });
+      };
+
+      AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+        var me = this;
+        autocomplete.bindTo('bounds', this.map);
+        autocomplete.addListener('place_changed', function() {
+          var place = autocomplete.getPlace();
+          if (!place.place_id) {
+            window.alert("Please select an option from the dropdown list.");
+            return;
+          }
+          if (mode === 'ORIG') {
+            me.originPlaceId = place.place_id;
+          } else {
+            me.destinationPlaceId = place.place_id;
+          }
+          me.route();
+        });
+      };
+
+      AutocompleteDirectionsHandler.prototype.route = function() {
+        if (!this.originPlaceId || !this.destinationPlaceId) {
+          return;
+        }
+        var me = this;
+
+        this.directionsService.route({
+          origin: {'placeId': this.originPlaceId},
+          destination: {'placeId': this.destinationPlaceId},
+          travelMode: this.travelMode
+        }, function(response, status) {
+          if (status === 'OK') {
+            me.directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      };
